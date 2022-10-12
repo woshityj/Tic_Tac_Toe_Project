22cc0b77d137e72061a94aca8c0286312a29327a
Joshua Lim <s10179502@connect.np.edu.sg>
2022-10-12 21:46:20 +0800

    added computer move and edited main for hard_mode

--- hard_mode.c
+++ hard_mode.c
@@ -14,6 +14,17 @@ const char PLAYER = 'X';
 const char COMPUTER = 'O';
 int turncounter = 0;
 
+static void printBoard()
+{
+    printf(" %c  | %c   | %c ", board[0][0], board[0][1], board[0][2]);
+    printf("\n--- | --- | --- \n ");
+    printf(" %c | %c   | %c ", board[1][0], board[1][1], board[1][2]);
+    printf("\n--- | --- | --- \n ");
+    printf(" %c | %c   | %c ", board[2][0], board[2][1], board[2][2]);
+    printf("\n--- | --- | --- \n ");
+    printf("\n");
+}
+
 static void playerMove (GtkWidget *widget, gpointer data)
 {
     int row, column;
@@ -151,7 +162,7 @@ int checkFreeSpaces()
     return freeSpaces;
 }
 
-int minimax(int board[3][3], int player) {
+int minimax(char board[3][3], int player) {
     //How is the position like for player (their turn) on board?
     int winner = win(board);
     if(winner != 0) return winner*player;
@@ -178,7 +189,7 @@ int minimax(int board[3][3], int player) {
     return score;
 }
 
-static void computerMove (GtkWidget *widget, gpointer data)
+static void computerMove (GtkWidget *widget)
 {
     int row = -1;
     int col = -1;
@@ -199,6 +210,78 @@ static void computerMove (GtkWidget *widget, gpointer data)
         }
     }
     //returns a score based on minimax tree at a given node.
-    board[row][col] = 1; // computer move
+    board[row][col] = COMPUTER; // computer move
+    gtk_button_set_label(GTK_BUTTON(widget),"O");
     printBoard();
+}
+
+static void resetBoard()
+{
+    memset(board, 0, sizeof(board));
+    gtk_button_set_label(GTK_BUTTON(button1), " ");
+    gtk_button_set_label(GTK_BUTTON(button2), " ");
+    gtk_button_set_label(GTK_BUTTON(button3), " ");
+    gtk_button_set_label(GTK_BUTTON(button4), " ");
+    gtk_button_set_label(GTK_BUTTON(button5), " ");
+    gtk_button_set_label(GTK_BUTTON(button6), " ");
+    gtk_button_set_label(GTK_BUTTON(button7), " ");
+    gtk_button_set_label(GTK_BUTTON(button8), " ");
+    gtk_button_set_label(GTK_BUTTON(button9), " ");
+}
+
+int main(int argc, char *argv[])
+{
+    gtk_init(&argc, &argv); //init Gtk
+
+    builder = gtk_builder_new_from_file("Tic_Tac_Toe_GUI.glade");
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
+
+    for (turncounter = 0; turncounter < 9 && win(board) == 0; ++turncounter){
+        if((turncounter) % 2 == 0){
+            computerMove(board);
+        }
+        else{
+            button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
+            g_signal_connect (button1, "clicked", G_CALLBACK (playerMove), "00");
+
+            button2 = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
+            g_signal_connect (button2, "clicked", G_CALLBACK (playerMove), "01");
+            
+            button3 = GTK_WIDGET(gtk_builder_get_object(builder, "button3"));
+            g_signal_connect (button3, "clicked", G_CALLBACK (playerMove), "02");
+            
+            button4 = GTK_WIDGET(gtk_builder_get_object(builder, "button4"));
+            g_signal_connect (button4, "clicked", G_CALLBACK (playerMove), "10");
+
+            button5 = GTK_WIDGET(gtk_builder_get_object(builder, "button5"));
+            g_signal_connect (button5, "clicked", G_CALLBACK (playerMove), "11");
+
+            button6 = GTK_WIDGET(gtk_builder_get_object(builder, "button6"));
+            g_signal_connect (button6, "clicked", G_CALLBACK (playerMove), "12");
+
+            button7 = GTK_WIDGET(gtk_builder_get_object(builder, "button7"));
+            g_signal_connect (button7, "clicked", G_CALLBACK (playerMove), "20");
+
+            button8 = GTK_WIDGET(gtk_builder_get_object(builder, "button8"));
+            g_signal_connect (button8, "clicked", G_CALLBACK (playerMove), "21");
+
+            button9 = GTK_WIDGET(gtk_builder_get_object(builder, "button9"));
+            g_signal_connect (button9, "clicked", G_CALLBACK (playerMove), "22");
+        }
+    }
+
+    resetbutton = GTK_WIDGET(gtk_builder_get_object(builder, "resetbutton"));
+    g_signal_connect (resetbutton, "clicked", G_CALLBACK (resetBoard), NULL);
+
+    player1 = GTK_WIDGET(gtk_builder_get_object(builder, "player1"));
+
+    gtk_widget_show(window);
+
+    gtk_main();
+
+    return EXIT_SUCCESS;
 }
